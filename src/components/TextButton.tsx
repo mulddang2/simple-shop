be32070a23101a2367b8e63a2e3f30007a52beef
Suzy-Lee <mulddang2@naver.com>
2023-08-16 00:29:28 +0900

    Fix: TextButton component

--- src/components/TextButton.tsx
+++ src/components/TextButton.tsx
@@ -2,51 +2,134 @@ import Button, { ButtonProps } from '@mui/material/Button';
 import { StyledEngineProvider } from '@mui/styled-engine';
 import styled from 'styled-components';
 
-enum TextButtonTheme {
-  CONTAINED,
+export enum TextButtonTheme {
+  DEFAULT,
   OUTLINED,
+  DARK,
+  BIG,
 }
 
-type TextButtonProps = ButtonProps & {
+const StyledDefaultButton = styled(Button)`
+  background-color: #661ae6;
+  border: 1px solid;
+  border-color: transparent;
+  border-radius: 8px;
+  color: #fff;
+  height: 3rem;
+  width: auto;
+  padding: 0px 1rem;
+  font-size: 0.875rem;
+  font-weight: 600;
+  line-height: 0.875rem;
+  letter-spacing: normal;
+
+  &:focus {
+    outline: none;
+  }
+  &:hover {
+    background-color: #5514b9;
+    color: #fff;
+    border-color: #5514b9;
+  }
+`;
+
+const StyledOutLineButton = styled(Button)`
+  background-color: transparent;
+  border: 1px solid;
+  border-color: #A6ADBB;
+  border-radius: 8px;
+  color: #A6ADBB;
+  height: 3rem;
+  width: auto;
+  padding: 0px 1rem;
+  font-size: 0.875rem;
+  font-weight: 600;
+  line-height: 0.875rem;
+  letter-spacing: normal;
+
+  &:focus {
+    outline: none;
+  }
+  &:hover {
+    background-color: #A6ADBB;
+    color: #2A303C;
+    border-color: #A6ADBB;
+  }
+`;
+const StyledDarkButton = styled(Button)`
+  background-color: #191d24;
+  border: 1px solid;
+  border-color: #191d24;
+  border-radius: 8px;
+  color: #a6adbb;
+  height: 3rem;
+  width: auto;
+  padding: 0px 1rem;
+  font-size: 0.875rem;
+  font-weight: 600;
+  line-height: 0.875rem;
+  letter-spacing: normal;
+
+  &:focus {
+    outline: none;
+  }
+  &:hover {
+    background-color: #111318;
+    color: #a6adbb;
+    border-color: #111318;
+  }
+`;
+
+const StyledBigButton = styled(Button)`
+  background-color: #661ae6;
+  border: 1px solid;
+  border-color: #661AE6;
+  border-radius: 8px;
+  color: #fff;
+  height: 4rem;
+  width: 10rem;
+  padding: 0px 1.5rem;
+  font-size: 1.125rem;
+  font-weight: 600;
+  letter-spacing: normal;
+
+  &:focus {
+    outline: none;
+  }
+  &:hover {
+    background-color: #5514b9;
+    color: #fff;
+    border-color: #5514b9;
+  }
+`;
+
+
+export type TextButtonProps = ButtonProps & {
   text: string;
-  theme?: TextButtonTheme;
+  theme: TextButtonTheme;
 };
 
 function TextButton({ text, theme, ...others }: TextButtonProps) {
-  if (theme === undefined) {
-    theme = TextButtonTheme.CONTAINED;
+  let button;
+
+  switch (theme) {
+    case TextButtonTheme.DEFAULT:
+      button = <StyledDefaultButton {...others}>{text}</StyledDefaultButton>;
+      break;
+
+    case TextButtonTheme.OUTLINED:
+      button = <StyledOutLineButton {...others}>{text}</StyledOutLineButton>;
+      break;
+
+    case TextButtonTheme.DARK:
+      button = <StyledDarkButton {...others}>{text}</StyledDarkButton>;
+      break;
+    case TextButtonTheme.BIG:
+      button = <StyledBigButton {...others}>{text}</StyledBigButton>;
+      break;
   }
 
-  const StyledButton = styled(Button)`
-    background-color: ${theme === TextButtonTheme.CONTAINED
-      ? '#570df8'
-      : 'transparent'};
-    border: 1px solid;
-    border-color: ${theme === TextButtonTheme.CONTAINED
-      ? 'transparent'
-      : 'rgb(166,173,187)'};
-    border-radius: 8px;
-    color: ${theme === TextButtonTheme.CONTAINED ? '#ffffff' : '#A6ADBB'};
-    height: 48px;
-    width: auto;
-    padding: 0px 16px;
-    font-size: 14px;
-    font-weight: 600;
-    line-height: 14px;
-    letter-spacing: normal;
-
-    &:focus {
-      outline: none;
-    }
-    &:hover {
-      background-color: #4406cb;
-    }
-  `;
-  return (
-    <StyledEngineProvider injectFirst>
-      <StyledButton {...others}>{text}</StyledButton>
-    </StyledEngineProvider>
-  );
+  return <StyledEngineProvider injectFirst>{button}</StyledEngineProvider>;
 }
 
-export { TextButton, TextButtonTheme };
+export default TextButton;
