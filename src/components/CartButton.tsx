2235923e4af8e81f8ab2e6b70293536389da1b8b
Suzy-Lee <mulddang2@naver.com>
2023-08-16 00:23:27 +0900

    Fix: CartButton component

--- src/components/CartButton.tsx
+++ src/components/CartButton.tsx
@@ -2,30 +2,32 @@ import { Badge, IconButton, IconButtonProps } from '@mui/material';
 import { StyledEngineProvider } from '@mui/styled-engine';
 import styled from 'styled-components';
 import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
+import { useAppSelector } from '../hooks/useRedux';
+import { getCartItemCount } from '../store/cart/CartSlice';
 
-type CartButtonProps = IconButtonProps & {
-  count: number;
-};
+const StyledIconButton = styled(IconButton)`
+  width: 3rem;
+`;
 
-function CartButton({ count }: CartButtonProps) {
-  const StyledIconButton = styled(IconButton)``;
+const StyledBadge = styled(Badge)`
+  color: #ffffff;
 
-  const StyledBadge = styled(Badge)`
-    color: #ffffff;
-    
-    > span {
-      background-color: #ef4444;
-    }
-  `;
+  > span {
+    background-color: #ef4444;
+  }
+`;
 
-  const StyledShoppingBagOutlinedIcon = styled(ShoppingBagOutlinedIcon)`
-    color: #ffffff;
-  `;
+const StyledShoppingBagOutlinedIcon = styled(ShoppingBagOutlinedIcon)`
+  color: #ffffff;
+`;
+
+function CartButton(props: IconButtonProps) {
+  const cartItems = useAppSelector(getCartItemCount);
 
   return (
     <StyledEngineProvider injectFirst>
-      <StyledIconButton>
-        <StyledBadge className="cart-count" badgeContent={count}>
+      <StyledIconButton {...props}>
+        <StyledBadge className="cart-count" badgeContent={cartItems}>
           <StyledShoppingBagOutlinedIcon />
         </StyledBadge>
       </StyledIconButton>
