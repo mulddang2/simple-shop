0a231633b3ce44089a8b7ddc8886284faf2e7592
Suzy-Lee <mulddang2@naver.com>
2023-08-16 00:23:58 +0900

    Fix: Link component

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,30 +1,43 @@
 import MuiLink, { LinkProps as MuiLinkProps } from '@mui/material/Link';
 import { StyledEngineProvider } from '@mui/styled-engine';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 
 type LinkProps = MuiLinkProps & {
   text: string;
+  href: string;
 };
 
-function Link({ text, ...others }: LinkProps) {
-  const StyledLink = styled(MuiLink)`
-    display: inline-block;
-    color: #374151;
-    font-size: 14px;
-    padding: 0 12px;
-    height: 32px;
-    line-height: 32px;
-    font-weight: 600;
-    text-decoration: none;
-    &:hover {
-      background-color: #1f293733;
-      border-radius: var(--rounded-btn, 0.5rem);
-    }
-  `;
+const StyledLink = styled(MuiLink)`
+  display: inline-block;
+  color: #fff;
+  /* color: #374151; */
+  font-size: 14px;
+  padding: 0 12px;
+  height: 32px;
+  line-height: 32px;
+  font-weight: 600;
+  text-decoration: none;
+  cursor: pointer;
+  &:hover {
+    background-color: #a6adbb33;
+    border-radius: var(--rounded-btn, 0.5rem);
+  }
+`;
+
+function Link({ text, href, ...others }: LinkProps) {
+  const navigate = useNavigate();
 
   return (
     <StyledEngineProvider injectFirst>
-      <StyledLink {...others}>{text}</StyledLink>
+      <StyledLink
+        onClick={() => {
+          navigate(href);
+        }}
+        {...others}
+      >
+        {text}
+      </StyledLink>
     </StyledEngineProvider>
   );
 }
