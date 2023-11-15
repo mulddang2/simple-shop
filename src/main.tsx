0c65d9746b7faca0c631d6795d806d86aac39fec
Suzy-Lee <mulddang2@naver.com>
2023-11-15 15:02:43 +0900

    fix: 빌드 시, 에러로 인한 네이밍 수정

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx';
 import './index.css';
 import { Provider } from 'react-redux';
 import store from './store/index.ts';
-import GlobalStyle from './styles/GlobalStyles.ts';
+import GlobalStyle from './styles/GlobalStyle.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
